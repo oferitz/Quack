d4df164af724030659e87d4b637fac971c6d1e4a
oferitz <ofer.webdev@gmail.com>
2024-01-22 20:16:22 +0200

    upgrade qwik

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useSignal, useStore } from '@builder.io/qwik'
 import { type DocumentHead, Link } from '@builder.io/qwik-city'
 
 export default component$(() => {
+  const store = useStore({ count: 0 })
   return (
     <div class="prose mt-32">
       <h1>Welcome to Quack</h1>
       <div>
         <Link href="/bookmarks">Bookmarks</Link>
       </div>
+        <div>
+            <button onClick$={() => store.count++}>+</button>
+            <div>{store.count}</div>
+            <button onClick$={() => store.count--}>-</button>
+        </div>
     </div>
   )
 })
